ae2d29c14ef17e7b97d28d4cf0772686b5e73966
Luis Lima <llimap5@miumg.edu.gt>
2024-08-31 18:55:27 -0600

    Practica de proxy

--- IntroduccionAJava/src/PracticaPatronesEstructurales/Proxy/PracticaDos/BookProxy.java
+++ IntroduccionAJava/src/PracticaPatronesEstructurales/Proxy/PracticaDos/BookProxy.java
@@ -3,16 +3,8 @@ package PracticaPatronesEstructurales.Proxy.PracticaDos;
 import java.util.ArrayList;
 import java.util.List;
 
-// el cliente interactua con el proxy y el proxy interactua con el objeto real
-// esto gracias a que el proxy tiene un atributo con el objeto real para poder
-// controlar su acceso
-public class BookProxy implements Book{ // el proxy siempre implementa la misma interfaz del objeto que
-    // va a representar
-    // en resumen, el proxy es el encargado de controlar las instancias del objeto que esta representando
-    private RealBook realBook; // este atributo que contiene el objeto real es null, porque como
-    // lo dijimos anteriormente, al momento de instanciar el objeto real, nos costara mucho tiempo o recursos
-    // entonces por eso le delegamos al metodo implementado de la interfaz (display) ese trabajo de referenciar
-    // o instanciar el objeto real y a su vez lo muestre
+public class BookProxy implements Book{
+    private RealBook realBook = null;
     private static final List<String> libros;
     private String title;
     private boolean isAuthorized;
@@ -24,32 +16,22 @@ public class BookProxy implements Book{ // el proxy siempre implementa la misma 
         libros.add("Games Of Thrones");
     }
 
-    // el cliente instancia el proxy, pero el proxy no instancia al objeto real
-    // hasta que se lo indiquemos con el metodo display, esto pasa porque
-    // el proxy no sabe si realmente quieres que te muestre el libro, y por ende, cargarlo antes de que te lo muestre
-    // porque es una tarea muy costosa, asi que el proxy esperara hasta que le indiques que realmente si
-    // quieres visualizar el libro para poder cargarlo y mostrartelo
     public BookProxy(String title, boolean isAuthorized) {
         this.isAuthorized = isAuthorized;
         this.title = title;
     }
 
-    // aqui estamos aplazando el metodo para cargar el libro desde el disco, porque
-    // es una tarea costosa para el programa, por lo tanto, solo lo haremos cuando sea necesario
-    // es decir, cuando queramos mostrarle el libro al cliente por medio del metodo display
     @Override
-    public void display() { // este metodo es el que delega la instancia hacia el objeto real
+    public void display() {
         if (isAuthorized) {
             if (libros.contains(title)) {
                 if (realBook == null) {
-                    // guarda una referencia con la carga del libro que ingresaste,
-                    // y se carga y muestra, solamente si lo pides
-                    realBook = new RealBook(title);
+                    this.realBook = new RealBook(title);
                 }
             } else {
                 throw new RuntimeException("No existe el libro en nuestra biblioteca");
             }
-            realBook.display();
+            this.realBook.display();
         } else {
             if (libros.contains(title)) {
                 System.out.println("Acceso denegado a: " + title);
--- IntroduccionAJava/src/PracticaPatronesEstructurales/Proxy/PracticaDos/RealBook.java
+++ IntroduccionAJava/src/PracticaPatronesEstructurales/Proxy/PracticaDos/RealBook.java
@@ -3,16 +3,11 @@ package PracticaPatronesEstructurales.Proxy.PracticaDos;
 public class RealBook implements Book{
     private String title;
 
-    // al momento de instanciar o referenciar el objeto real, nos tomara mas recursos
-    // que si lo hacemos mediante el representante del proxy, porque el proxy solo
-    // actuara solo si nosotros le indicamos que queremos ver el libro, entonces ahi
-    // proxy ya crea la referencia al objeto real y a su vez lo muestra con el metodo display
     public RealBook(String title) {
         this.title = title;
         loadFromDisk();
     }
 
-    // este metodo representa una tarea costosa al momento de crear la instancia del libro
     public void loadFromDisk(){
         System.out.println("Cargando libro: " + title);
     }
